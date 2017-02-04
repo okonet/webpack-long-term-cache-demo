0ec7a0822c6602bb26c4a81b920e9bad92d0a8de
andresgutgon <andresgutgon@gmail.com>
2017-02-04 13:02:20 +0100

    ID with route prefix

--- src/Routes/routeA/childComponent/component/index.jsx
+++ src/Routes/routeA/childComponent/component/index.jsx
@@ -6,7 +6,7 @@ export default class ChildComponent extends React.Component {
   render () {
     return (
       <div>
-        Here we're with params ID: <strong>{this.props.params.id}</strong>
+        Here we're with params ID: <strong>route-A-ID-{this.props.params.id}</strong>
       </div>
     )
   }
